07c6e93fa5dfe920e121cca54ee451b4afde3054
周培公 <peigong@foxmail.com>
2014-02-08 20:34:34 +0800

    增加log4js日志和服务启动钩子。

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,49 +12,15 @@ module.exports = function (grunt) {
       server: {
         files: [ 
           { expand: true, cwd: "lib", src: ['**'], dest: 'server' },
-          { expand: true, cwd: "modules", src: ['**'], dest: 'server/modules' }
+          { expand: true, cwd: "tests", src: ['**'], dest: 'server' }
         ]
       }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  // Default task.
-  grunt.registerTask('default', ['clean', 'copy']);//'jshint', 'nodeunit', 
 
+  // Default task.
+  grunt.registerTask('default', ['clean', 'copy']);
 };
---
+++ tests/hooks/start/hook_start_test.js
@@ -0,0 +1,4 @@
+module.exports = function (data) {
+    data = data || {};
+    console.log(data);
+};
